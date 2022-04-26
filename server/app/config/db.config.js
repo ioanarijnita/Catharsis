dc6cc7b5bd73660a9adb7c3e1db5c490d745f57c
andreinegrea14 <andrei.negrea14@yahoo.com>
2022-04-27 00:16:20 +0300

    changed server cors policy & host, name, pass, db

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "catharsis",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "eu-cdbr-west-02.cleardb.net",
+  USER: "b921ce9dbaf738",
+  PASSWORD: "5286107e",
+  DB: "heroku_1ccfee457fd604a",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
