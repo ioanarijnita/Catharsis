fcea046b8d61647205ae6bf4dacf09d5c232b3fb
Ioana <ioanarijnita@gmail.com>
2022-05-16 21:57:11 +0300

    changed footer layout

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,17 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
-    const footerItems = ["Catharsis © 2022", "Contact", "Social"];
+    const footerItems = ["Catharsis © 2022", "Contact"];
     return (
         <div style={{ marginTop: 10, marginBottom: 10, bottom: 0, width: "100%" }}>
             <hr />
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-around" }}>
                 {footerItems.map(item => <span key={item} onClick={() => { }} style={{ marginLeft: 20, cursor: "pointer" }}>{item}</span>)}
                 <a href="https://www.facebook.com/profile.php?id=100009870003700"
                     className="facebook social"
                     style={{ color: "blue", marginLeft: 10 }}
                 >
-                    <FontAwesomeIcon icon={faFacebook} size="1x" />
+                    <FontAwesomeIcon icon={faFacebook} size="1x" color="#2196f3" />
                 </a>
             </div>
         </div>
